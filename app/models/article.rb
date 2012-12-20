c715e22bd2c8a9c3975444d629823b46c5bffdc6
Howard Fackrell <howard.fackrell@octanner.com>
2012-12-20 12:09:54 -0700

    add spec tests for article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ActiveRecord::Base
   include ActiveModel::ForbiddenAttributesProtection
+
+  validates :title, length:  { minimum: 1}
+  validates :body, length: { minimum: 3}
+
   has_many :comments
 end
