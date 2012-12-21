debc2922f2f1cb9afc99f0233a48cc2fa702de5d
Howard Fackrell <howard.fackrell@octanner.com>
2012-12-21 09:44:47 -0700

    remove rails generated index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Blogger::Application.routes.draw do
   resources :articles
   resources :comments
+
+  root to: 'articles#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
